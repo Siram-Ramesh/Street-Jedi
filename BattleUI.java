d494d143767e843efdc56971c7031539eeef4e36
flame539275 <sriramramesh@sfhs.com>
2016-05-07 22:32:59 -0700

    Update BattleUI.java

--- BattleUI.java
+++ BattleUI.java
@@ -1,4 +1,5 @@
 
+
 import java.awt.Event;
 import javax.swing.*;
 import java.awt.*;
@@ -7,8 +8,17 @@ public class BattleUI
 {
     Character player1;
     Character player2;
+    JFrame frame;
     public BattleUI(int p1, int p2)
     {
+        frame = new JFrame("Street-Jedi");
+        frame.setSize(800,600);
+        player1 = new Luke(frame);
+        player2 = new Luke(frame);
+        frame.add(player1);
+        frame.add(player2);
+        frame.setVisible(true);
+        /*
         switch (p1)
         {
             case 0://player1 = new 
@@ -30,6 +40,7 @@ public class BattleUI
             case 3://player2 = new
             break;
         }  
+        */
         class ControlsListener implements KeyListener
         {
             public void keyPressed(KeyEvent e)
@@ -47,8 +58,12 @@ public class BattleUI
                     break;
                     case KeyEvent.VK_S: player1.block(); break;
                     case KeyEvent.VK_K: player2.block(); break;
-                    //case KeyEvent.VK_D: rC.decreaseB(); break;
-                    //case KeyEvent.VK_W: rC.increaseG(); break;
+                    case KeyEvent.VK_D: if(!(player1.isBlocking())) player1.changeXSpeed(15);  break;
+                    case KeyEvent.VK_A: if(!(player1.isBlocking())) player1.changeXSpeed(-15); break;
+                    case KeyEvent.VK_W: if(!(player1.isBlocking())) player1.jump(15);  break;
+                    case KeyEvent.VK_J: if(!(player2.isBlocking())) player2.changeXSpeed(-15);  break;
+                    case KeyEvent.VK_L: if(!(player2.isBlocking())) player2.changeXSpeed(15);  break;
+                    case KeyEvent.VK_I: if(!(player1.isBlocking())) player2.jump(15);  break;
                 }
             }
 
@@ -58,11 +73,15 @@ public class BattleUI
                 {
                     case KeyEvent.VK_S: player1.stopBlocking(); break;
                     case KeyEvent.VK_K: player2.stopBlocking(); break;
+                    case KeyEvent.VK_D: player1.stopWalking(); break;
+                    case KeyEvent.VK_A: player1.stopWalking(); break;
+                    case KeyEvent.VK_J: player2.stopWalking(); break;
+                    case KeyEvent.VK_L: player2.stopWalking(); break;
                 }
             }
 
             public void keyTyped(KeyEvent e){}   
         }
+        frame.addKeyListener(new ControlsListener());
     }
-
 }
