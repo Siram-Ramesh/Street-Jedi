7c713379517d0ccf8b8df2a75358c684923ba6c5
flame539275 <sriramramesh@sfhs.com>
2016-05-13 10:40:22 -0700

    Update Luke.java

--- Luke.java
+++ Luke.java
@@ -1,60 +1,51 @@
 import java.awt.*;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.awt.geom.*;
 public class Luke extends Character
 {
-    BufferedImage standing;
-    BufferedImage walking;
-    BufferedImage blocking;
-    BufferedImage jumping;
-    BufferedImage attacking;
-    JFrame frame;
-    public Luke(JFrame fr)
+    ImageIcon standing;
+    ImageIcon walking;
+    ImageIcon blocking;
+    ImageIcon jumping;
+    ImageIcon attacking;
+    ImageIcon luke;
+    public Luke()
     {
-        frame = fr;
         super.height = 50;
-        try {
-            standing = ImageIO.read(new File("lukeStand.gif"));
-        } catch (IOException e) {
-        }
-        try {
-            walking = ImageIO.read(new File("lukeWalk.gif"));
-        } catch (IOException e) {
-        }
-        try {
-            attacking = ImageIO.read(new File("lukeSlash1fast.gif"));
-        } catch (IOException e) {
-        }
-        try {
-            standing = ImageIO.read(new File("lukeJump.gif"));
-        } catch (IOException e) {
-        }
+        standing = new ImageIcon("lukeStand.gif");
+        walking = new ImageIcon("lukeWalk.gif");
+        attacking = new ImageIcon("lukeSlash1fast.gif");
+        standing = new ImageIcon("lukeJump.gif");
     }
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        if(super.blocking)
+        if(!(super.blocking))
         {
             //g2.drawImage((Image)blocking, 0,0, null);
+            g2.setColor(Color.GREEN);
+            Rectangle2D.Double placeHolder = new Rectangle2D.Double(300,300,50,50);
+            System.out.println("Rectbgle");
+            g2.draw(placeHolder);
         }
         else
         {
             if(super.yloc < (500- super.height))
             {
-                g2.drawImage((Image)jumping, 0,0, null);
+                
             }
             else
             {
                 if(super.walking)
                 {
-                    g2.drawImage((Image)walking, 0,0, null);
+                    
                 }
                 else
                 {
-                    g2.drawImage((Image)standing, 0,0, null);
+                    
                 }
             }
         }
